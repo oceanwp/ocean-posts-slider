1ff798d8e39f839ff5eab6478764c1d3b30c8edf
eramits <apprimit@gmail.com>
2022-02-01 10:11:44 +0530

    updated version

--- ocean-posts-slider.php
+++ ocean-posts-slider.php
@@ -3,11 +3,11 @@
  * Plugin Name:         Ocean Posts Slider
  * Plugin URI:          https://oceanwp.org/extension/ocean-posts-slider/
  * Description:         Display your latest posts in a beautiful slider with different options.
- * Version:             2.0.2
+ * Version:             2.0.3
  * Author:              OceanWP
  * Author URI:          https://oceanwp.org/
  * Requires at least:   5.3
- * Tested up to:        5.8
+ * Tested up to:        5.9
  *
  * Text Domain: ocean-posts-slider
  * Domain Path: /languages
@@ -91,7 +91,7 @@ final class Ocean_Posts_Slider {
 		$this->token       = 'ocean-posts-slider';
 		$this->plugin_url  = plugin_dir_url( __FILE__ );
 		$this->plugin_path = plugin_dir_path( __FILE__ );
-		$this->version     = '2.0.2';
+		$this->version     = '2.0.3';
 
 		register_activation_hook( __FILE__, array( $this, 'install' ) );
 
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: oceanwp, apprimit, wpfleek
 Tags: slider, posts slider, oceanwp
 Requires at least: 5.3
-Tested up to: 5.8
-Stable tag: 2.0.2
+Tested up to: 5.9
+Stable tag: 2.0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -31,6 +31,9 @@ This plugin will only work with the [OceanWP](https://oceanwp.org/) theme.
 3. Three Images Style.
 
 == Changelog ==
+
+= 2.0.3 =
+- Added: Updated: WordPress version number for compatibility.
 
 = 2.0.2 =
 - Fixed: Navigation: arrows.
